3047f546d80b115f19f8444c4b143667ded18ce2
Maksym Korniyko <korniykom@gmail.com>
2024-11-15 21:19:52 +0200

    feat: plugin that makes hoes useful by givin em ability to shoot

---
+++ GunsPlugin/src/main/java/com/korniykom/gunsPlugin/GunsPlugin.java
@@ -0,0 +1,50 @@
+package com.korniykom.gunsPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GunsPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+
+        Player player = event.getPlayer();
+
+        if(event.getHand().equals(EquipmentSlot.HAND)) {
+            if(event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                if (player.getInventory().getItemInMainHand().getType().equals(Material.WOODEN_HOE)) {
+                    player.launchProjectile(ThrownExpBottle.class, player.getLocation().getDirection());
+                } else if (player.getInventory().getItemInMainHand().getType().equals(Material.STONE_HOE)) {
+                    player.launchProjectile(WitherSkull.class, player.getLocation().getDirection());
+                } else if (player.getInventory().getItemInMainHand().getType().equals(Material.IRON_HOE)) {
+                    player.launchProjectile(Trident.class, player.getLocation().getDirection());
+                }else if (player.getInventory().getItemInMainHand().getType().equals(Material.GOLDEN_HOE)) {
+                    player.launchProjectile(Fireball.class, player.getLocation().getDirection());
+                } else if (player.getInventory().getItemInMainHand().getType().equals(Material.DIAMOND_HOE)) {
+                    player.launchProjectile(SpectralArrow.class, player.getLocation().getDirection());
+                } else if (player.getInventory().getItemInMainHand().getType().equals(Material.NETHERITE_HOE)) {
+                    player.launchProjectile(BreezeWindCharge.class, player.getLocation().getDirection());
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
